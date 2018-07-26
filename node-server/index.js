329cc401d1dc782eb35b49a264843106d0c8f585
brianlee <blees.email@gmail.com>
2018-07-26 10:50:38 -0400

    Render HTML template with data

--- node-server/index.js
+++ node-server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', async (req, res) => {
-  res.send('Hello World!');
   const url = 'http://127.0.0.1:5000/status';
   const {data} = await axios.get(url);
   console.log(data);
+  res.render('index', data);
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
