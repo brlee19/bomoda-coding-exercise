1dd3eb4491717e82d3562fdb027a0f0c72068cd5
brianlee <blees.email@gmail.com>
2018-07-26 09:49:34 -0400

    API meets specs

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,28 +1,48 @@
 from flask import Flask
-import json, time
+from datetime import datetime
+from time import time
+import json
 
 app = Flask(__name__)
 
-# read persisent_time_run and _first_ever_start from file
-# if file is empty, init the file
+# get data from data.json if the server has been run before
+# or initialize it if this is the first time the server is being run
+try:
+    persisted_data = open('data.json', 'r')
+    server_data = json.load(persisted_data)
+    server_first_start = server_data['server_first_start']
+    server_endpoint_times_requested = server_data['server_endpoint_times_requested']
+    persisted_data.close()
+except FileNotFoundError:
+    server_first_start = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    server_endpoint_times_requested = 0
 
+# dictionary with all app data
 times_run = 0
-start_time = time.time()
+start_time = time()
 app_data = {
-    'server_first_start': None,
-    'server_times_run': 0,
-    'process_times_run': 0,
     'process_elapsed_time': 0,
+    'process_endpoint_times_requested': 0,
+    'server_first_start': server_first_start,
+    'server_endpoint_times_requested': server_endpoint_times_requested,
 }
 
 
 @app.route('/status')
 def index():
-    global app_data, start_time
-    # update process_times_run
-    # update persistent_times_run and in the filesystem
-    app_data['process_times_run'] += 1
-    app_data['process_elapsed_time'] = time.time() - start_time
+    # update app data
+    global app_data, start_time, server_first_start
+    app_data['process_endpoint_times_requested'] += 1
+    app_data['process_elapsed_time'] = time() - start_time
+    app_data['server_endpoint_times_requested'] += 1
+
+    # update data.json
+    with open('data.json', 'w') as data_file:
+        json.dump({
+            'server_first_start': server_first_start,
+            'server_endpoint_times_requested': app_data['server_endpoint_times_requested']
+        }, data_file)
+
     return json.dumps(app_data)
 
 
