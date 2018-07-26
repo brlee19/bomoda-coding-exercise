0e668e5ba2580bdf7f3da7fa5af31060510da10b
brianlee <blees.email@gmail.com>
2018-07-26 10:26:47 -0400

    Node server getting data from flask api

--- node-server/index.js
+++ node-server/index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const axios = require('axios');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  res.send('Hello World!');
+  const url = 'http://127.0.0.1:5000/status';
+  const {data} = await axios.get(url);
+  console.log(data);
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('App listening on port 3000!'));
